14a7628fa11cd741fe307a4522c00451b2ae8e45
pengeorge <zp-chen11@mails.tsinghua.edu.cn>
2015-10-15 01:01:48 +0800

    cross-compilable

---
+++ Tunet/autogen-cross-compile.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+aclocal
+automake --add-missing
+autoheader
+sed -i 's:\(^#undef malloc$\|^#undef realloc$\):/* \1 */:' config.h.in
+autoconf
+./configure --build=`./config.guess` --host=mips-openwrt-linux-uclibc
+
