6ea72f49bab9dbae64fc65075f8098ab3007a5c5
pengeorge <zp-chen11@mails.tsinghua.edu.cn>
2015-11-12 02:15:32 +0800

    fix .travis.yml

--- Tunet/autogen-cross-compile.sh
+++ Tunet/autogen-cross-compile.sh
@@ -4,5 +4,5 @@ automake --add-missing
 autoheader
 sed -i 's:\(^#undef malloc$\|^#undef realloc$\):/* \1 */:' config.h.in
 autoconf
-./configure --build=`./config.guess` --host=mips-openwrt-linux-uclibc
+#./configure --build=`./config.guess` --host=mips-openwrt-linux-uclibc
 
